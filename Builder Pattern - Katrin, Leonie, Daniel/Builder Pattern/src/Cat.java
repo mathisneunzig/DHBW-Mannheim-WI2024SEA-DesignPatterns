ca5c77f70e02bf19153d6188ec572e6e54dc54a5
DanielBrecht-dhbw <s241293@student.dhbw-mannheim.de>
2025-09-28 18:10:03 +0200

    finished CatBuilder

--- Builder Pattern - Katrin, Leonie, Daniel/Builder Pattern/src/Cat.java
+++ Builder Pattern - Katrin, Leonie, Daniel/Builder Pattern/src/Cat.java
@@ -2,6 +2,56 @@ import CatEnums.*;
 
 public class Cat {
 
+    public static class CatBuilder{
+        Cat cat = new Cat();
+        public CatBuilder(){
+        }
+
+        public CatBuilder setName(String name){
+            cat.setName(name);
+            return this;
+        }
+
+        public CatBuilder setAge(int age){
+            cat.setAge(age);
+            return this;
+        }
+
+        public CatBuilder setRace(CatRace race){
+            cat.setRace(race);
+            return this;
+        }
+
+        public CatBuilder setFurColor(CatFurColor furColor){
+            cat.setFurColor(furColor);
+            return this;
+        }
+
+        public CatBuilder setFurPattern(CatFurPattern furPattern){
+            cat.setFurPattern(furPattern);
+            return this;
+        }
+
+        public CatBuilder setEyeColor(CatEyeColor eyeColor){
+            cat.setEyeColor(eyeColor);
+            return this;
+        }
+
+        public CatBuilder setFavouriteToy(CatToy favouriteToy){
+            cat.setFavouriteToy(favouriteToy);
+            return this;
+        }
+
+        public CatBuilder setFavouriteConsumable(CatConsumable favouriteConsumable){
+            cat.setFavouriteConsumable(favouriteConsumable);
+            return this;
+        }
+
+        public Cat build(){
+            return cat;
+        }
+    }
+
     public static final String DEFAULT_NAME = "Mietzi";
     public static final int DEFAULT_AGE = 1;
     public static final CatPersonality DEFAULT_PEERSONALITY = CatPersonality.SLEEPY;
@@ -22,13 +72,8 @@ public class Cat {
     private CatToy favouriteToy;
     private CatConsumable favouriteConsumable;
 
-    public Cat(CatPersonality personality){
-        this.personality = personality;
-    }
-
-    //private Cat(){}
-
-    public static class CatBuilder{
+    private Cat(){
+        name = DEFAULT_NAME;
     }
 
     public String getName() {
@@ -53,5 +98,53 @@ public class Cat {
 
     public void setPersonality(CatPersonality personality) {
         this.personality = personality;
+    }
+
+    public CatRace getRace() {
+        return race;
+    }
+
+    public void setRace(CatRace race) {
+        this.race = race;
+    }
+
+    public CatFurColor getFurColor() {
+        return furColor;
+    }
+
+    public void setFurColor(CatFurColor furColor) {
+        this.furColor = furColor;
+    }
+
+    public CatFurPattern getFurPattern() {
+        return furPattern;
+    }
+
+    public void setFurPattern(CatFurPattern furPattern) {
+        this.furPattern = furPattern;
+    }
+
+    public CatEyeColor getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(CatEyeColor eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public CatToy getFavouriteToy() {
+        return favouriteToy;
+    }
+
+    public void setFavouriteToy(CatToy favouriteToy) {
+        this.favouriteToy = favouriteToy;
+    }
+
+    public CatConsumable getFavouriteConsumable() {
+        return favouriteConsumable;
+    }
+
+    public void setFavouriteConsumable(CatConsumable favouriteConsumable) {
+        this.favouriteConsumable = favouriteConsumable;
     }
 }
--- Builder Pattern - Katrin, Leonie, Daniel/Builder Pattern/src/Main.java
+++ Builder Pattern - Katrin, Leonie, Daniel/Builder Pattern/src/Main.java
@@ -2,6 +2,5 @@ import CatEnums.CatPersonality;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Cat(CatPersonality.CUDLY).getPersonality());
     }
 }
