34d92839358e1f4e7f5cbf339558b3fec1ef1213
DanielBrecht-dhbw <s241293@student.dhbw-mannheim.de>
2025-09-28 15:26:07 +0200

    added all DEFAULT constants

--- Builder Pattern - Katrin, Leonie, Daniel/Builder Pattern/src/Cat.java
+++ Builder Pattern - Katrin, Leonie, Daniel/Builder Pattern/src/Cat.java
@@ -1,11 +1,26 @@
-import CatEnums.CatPersonality;
+import CatEnums.*;
 
 public class Cat {
+
+    public static final String DEFAULT_NAME = "Mietzi";
+    public static final int DEFAULT_AGE = 1;
+    public static final CatPersonality DEFAULT_PEERSONALITY = CatPersonality.SLEEPY;
+    public static final CatRace DEFAULT_RACE = CatRace.CHARTREUX;
+    public static final CatFurColor DEFAULT_FUR_COLOR = CatFurColor.GREY;
+    public static final CatFurPattern DEFAULT_FUR_PATTERN = CatFurPattern.STRIPED;
+    public static final CatEyeColor DEFAULT_EYE_COLOR = CatEyeColor.BLUE;
+    public static final CatToy DEFAULT_FAVOURITE_TOY = CatToy.LASER_POINTER;
+    public static final CatConsumable DEFAULT_FAVOURITE_CONSUMABLE = CatConsumable.TUNA;
 
     private String name;
     private int age;
     private CatPersonality personality;
-
+    private CatRace race;
+    private CatFurColor furColor;
+    private CatFurPattern furPattern;
+    private CatEyeColor eyeColor;
+    private CatToy favouriteToy;
+    private CatConsumable favouriteConsumable;
 
     public Cat(CatPersonality personality){
         this.personality = personality;
