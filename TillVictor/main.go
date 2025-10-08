b49f02def13f5ac755a6fc88049b4760aa7d5b42
Till Burdorf <burdorftill@gmail.com>
2025-10-08 12:29:19 +0200

    eddited commenting

--- TillVictor/main.go
+++ TillVictor/main.go
@@ -6,22 +6,23 @@ import "fmt"
 type MikroUSBStecker struct{}
 
 func (a *MikroUSBStecker) MikroUSBEinstecken() {
-	fmt.Println("Microsteckt drin")
+	fmt.Println("Micro USB Stecker steckt drin.")
 }
 
 // USB-C Stecker unterstützt USBCEinstecken
 type USBCStecker struct{}
 
 func (e *USBCStecker) USBCEinstecken() {
-    fmt.Println("Echter USB-C steckt drin.")
+	fmt.Println("Echter USB-C Stecker steckt drin.")
 }
 
-// Der Adapter hat einen microUSbStecker und implementiert das Interface USBC
+// Der Adapter hat einen microUSbStecker und implementiert das Interface IUSBCStecker
 type Adapter struct {
 	microUSBStecker *MikroUSBStecker
 }
 
 func (s *Adapter) USBCEinstecken() {
+	fmt.Println("Stecke Micro USB Stecker in den Adapter...")
 	s.microUSBStecker.MikroUSBEinstecken()
 }
 
@@ -30,30 +31,34 @@ type IUSBCStecker interface {
 	USBCEinstecken()
 }
 
+// Die Funktion erwartet ein Usbc-Gerät und Steckt dann den USBC Stecker ein
+type Client struct {
+	MicroUSBStecker *MikroUSBStecker
+	USBCStecker     *USBCStecker
+}
 
-//Die Funktion erwartet ein Usbc-Gerät und Steckt dann den USBC Stecker ein
-type Client struct{}
-
-func (c *Client) SteckerEinstecken(stecker IUSBCStecker){
-    stecker.USBCEinstecken()
+func (c *Client) SteckerEinstecken(stecker IUSBCStecker) {
+	stecker.USBCEinstecken()
 }
 
 func main() {
-    //Der Client möchte gerne alle seine Stecker einstecken, 
-    //kennt aber nur das Interface IUSBCStecker mit der Funktion USBC-Einstecken
-    Client := &Client{}
+	//Der Client möchte gerne alle seine Stecker einstecken,
+	//kennt aber nur das Interface IUSBCStecker mit der Funktion USBC-Einstecken
+	Client := &Client{
+		MicroUSBStecker: &MikroUSBStecker{},
+		USBCStecker:     &USBCStecker{},
+	}
 
-    //seinen neuen USBC Stecker kann er Problemlos einstecken...
-    var usbcStecker *USBCStecker
-    Client.SteckerEinstecken(usbcStecker)
+	//seinen neuen USBC Stecker kann er Problemlos einstecken...
+	Client.SteckerEinstecken(Client.USBCStecker)
 
-    //aber beim Versuch seinen alten microUSB Stecker einzustecken gibt es einen Fehler...
-    var microUSBStecker *MikroUSBStecker
-    //Client.SteckerEinstecken(microUSBStecker) wirft einen Fehler
+	//aber beim Versuch seinen alten microUSB Stecker einzustecken gibt es einen Fehler...
+	//Client.SteckerEinstecken(Client.MicroUSBStecker) würde einen Fehler werfen,
+	// weil MicroUSBStecker das Interface IUSBCStecker nicht implementiert
 
-    //Deswegen steckt er seinen alten Mikro USB stecker in den Adapter und versucht es nochmal
-    adapter := &Adapter{microUSBStecker: microUSBStecker}
-    Client.SteckerEinstecken(adapter)
+	//Deswegen steckt er seinen alten Mikro USB stecker in den Adapter und versucht es nochmal
+	adapter := &Adapter{microUSBStecker: Client.MicroUSBStecker}
+	Client.SteckerEinstecken(adapter)
 
-    //Jetzt gibt es keinen Fehler mehr und das einstecken Funktioniert auch mit dem Mikro-USB Stecker
+	//Jetzt gibt es keinen Fehler mehr und das einstecken Funktioniert auch mit dem Mikro-USB Stecker
 }
